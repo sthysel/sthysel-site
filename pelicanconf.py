678442bed20770c0d9762be4c08a05d3f69ecb6b
Thys Meintjes <matthys.meintjes@bhp.com>
2019-08-04 18:49:08 +0800

    init

---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'sthysel'
+SITENAME = 'the daily doomer'
+SITESUBTITLE = 'observations and rumifications on shit'
+SITEURL = ''
+GITHUB_URL = 'http://github.com/sthysel/'
+PATH = 'content'
+
+TIMEZONE = 'Australia/Perth'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (("behold the whore of babylon's purse", 'http://github.com/sthysel'),
+         ('control is what we crave', 'http://github.com/sthysel-freight'),
+         ('carbohydrate carcass desecration', 'http://github.com/sthysel-shop'),
+         ('zsh, lets not talk about this', 'http://github.com/sthysel-zsh'),
+         )
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['./pelican-plugins']
+PLUGINS = ['gravatar']
+
